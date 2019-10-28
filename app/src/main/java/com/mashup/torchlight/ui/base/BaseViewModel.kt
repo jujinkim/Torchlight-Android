c39e64a4ca699db3910393f8e05818dffaf93b62
leejinseong <dlwls5201@naver.com>
2019-10-29 01:23:15 +0900

    Add base view

---
+++ app/src/main/java/com/mashup/torchlight/ui/base/BaseViewModel.kt
@@ -0,0 +1,14 @@
+package com.mashup.torchlight.ui.base
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+abstract class BaseViewModel : ViewModel() {
+
+    val compositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        compositeDisposable.clear()
+        super.onCleared()
+    }
+}
