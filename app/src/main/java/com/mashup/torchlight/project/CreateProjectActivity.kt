ca72a19e292b0dbd18218ced02beb5be1e974065
YuChocopie <rladbwjd341@naver.com>
2019-10-31 03:32:54 +0900

    feat: Add Project activity and global btn
    - `project activity` 생성
    - 공통으로 쓰일 상단, 하단 바 생성
    
    Fixed #4

---
+++ app/src/main/java/com/mashup/torchlight/project/CreateProjectActivity.kt
@@ -0,0 +1,13 @@
+package com.mashup.torchlight.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mashup.torchlight.R
+
+class CreateProjectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_project)
+    }
+}
