59151d789a914ab023e69555d68b6cbd2fee7603
YuChocopie <rladbwjd341@naver.com>
2019-10-31 09:17:53 +0900

    chore: add btn customview
    - 버튼3개사용부분 커스텀뷰 생성 //추후 text칸 추가 등 요청해주세요
    - 카테고리 리스트 나열 목록 생성

--- app/src/main/java/com/mashup/torchlight/ui/project/CreateProjectActivity.kt
+++ app/src/main/java/com/mashup/torchlight/ui/project/CreateProjectActivity.kt
@@ -1,13 +1,52 @@
 package com.mashup.torchlight.ui.project
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
 import com.mashup.torchlight.R
+import com.mashup.torchlight.adapter.CategoryAdapter
+import kotlinx.android.synthetic.main.activity_create_project.*
+
 
 class CreateProjectActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_project)
+
+        //TODO:: categoty 부분 + 버튼3개 커스텀뷰 테스트코드입니당 쓰실경우 이거 쓰면 되고 나중에 지울게요!
+        rvCategory.layoutManager = FlexboxLayoutManager(this)
+        val layoutManager = FlexboxLayoutManager(this)
+        layoutManager.flexDirection = FlexDirection.ROW
+        layoutManager.justifyContent = JustifyContent.FLEX_START
+        rvCategory.layoutManager = layoutManager
+
+        val categoryList = mutableListOf(
+            "ononeonee",
+            "two",
+            "oneoneone",
+            "two",
+            "one",
+            "onetwo",
+            "one",
+            "onetwo",
+            "one",
+            "two"
+        )
+
+        val adapter = CategoryAdapter(categoryList)
+        rvCategory.adapter = adapter
+        adapter.notifyDataSetChanged()
+
+        btnSelectThree.setText("단계","2단계","3단계")
+        btnSelectThree.setOnButtonClick(object : CustomThreeBtn.OnButtonClick {
+            override fun onSelect(index: Int) {
+                btnSelectThree.setCheckId(index)
+                Log.e("123123", "" + btnSelectThree.getCheckId())
+            }
+        })
     }
 }
