8682b51633f30fc15f1899354336d553d03a608f
Jujin <jujin@jujinkim.com>
2019-11-07 03:05:05 +0900

    feat: initialize and implement some methods in SignUpViewModel feat: add SignUpWork class

--- app/src/main/java/com/mashup/torchlight/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/com/mashup/torchlight/viewmodel/SignUpViewModel.kt
@@ -1,7 +1,42 @@
 package com.mashup.torchlight.viewmodel
 
-import androidx.lifecycle.ViewModel
+import androidx.databinding.ObservableField
+import com.mashup.torchlight.model.SignUpModel
+import com.mashup.torchlight.work.SignUpWork
 
-class SignUpViewModel() : ViewModel() {
+class SignUpViewModel : BaseViewModel() {
+    var data: SignUpModel =
+        SignUpModel("","","","","",false)
+
+    val sendAuthBtnEnabled = ObservableField(false)
+
+    val signUpWork = SignUpWork()
+
+    fun isPasswordConfirmed() : Boolean {
+        return data.passwd == data.passwdConfirm
+    }
+
+    fun isAuthCodeSame(input: String): Boolean {
+        return input == data.authCode
+    }
+
+    fun isBasicInfoFilled(): Boolean {
+        return data.emailAddress.isNotEmpty()
+                && data.name.isNotEmpty()
+                && data.passwd.isNotEmpty()
+    }
+
+    fun checkDuplicatedEmail() {
+        sendAuthBtnEnabled.set(!signUpWork.isDuplicatedEmail(data.emailAddress))
+    }
+
+    fun isPasswordConditionMatched(): Boolean {
+        /*
+        Password Condition:
+        one more alphabet + one more number + one more special character + 10 or over characters
+        */
+
+        return true
+    }
 
 }
