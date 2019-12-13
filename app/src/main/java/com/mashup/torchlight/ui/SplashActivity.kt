3af5ecd7683582df979c5f58e6284838273236d2
Jujin <jujin@jujinkim.com>
2019-12-14 01:37:39 +0900

    feat: implement Splash screen remove splash layout add splash theme style and download add TorchlightInitializer

--- app/src/main/java/com/mashup/torchlight/ui/SplashActivity.kt
+++ app/src/main/java/com/mashup/torchlight/ui/SplashActivity.kt
@@ -2,12 +2,34 @@ package com.mashup.torchlight.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mashup.torchlight.R
+import com.mashup.torchlight.ui.signin.SignInActivity
+import com.mashup.torchlight.work.initializer.ITorchlightInitializerListener
+import com.mashup.torchlight.work.initializer.TorchlightInitializer
+import org.jetbrains.anko.startActivity
+import timber.log.Timber
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), ITorchlightInitializerListener {
+
+    private val initializer = TorchlightInitializer(this)
+    private var initStartTimeStamp = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        initializer.startInit()
+    }
+
+    override fun onInitializeBegin() {
+
+        Timber.v("Start Torchlight initialize")
+        initStartTimeStamp = System.currentTimeMillis()
+    }
+
+    override fun onInitializeDone() {
+        val initPeriod = System.currentTimeMillis() - initStartTimeStamp
+        Timber.v("Finish Torchlight initialize. It took $initPeriod")
+
+        // Todo : If there is already a signed in user account, skip SignInActivity and go to MainActivity directly
+        startActivity<SignInActivity>()
+        finish()
     }
 }
