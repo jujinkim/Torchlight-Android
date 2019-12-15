e3e7a648bb97cae941f47da449075aec3f5a58e1
Jujin <jujin@jujinkim.com>
2019-12-15 15:41:03 +0900

    feat: set onClickListeners at SignInActivity.kt

--- app/src/main/java/com/mashup/torchlight/ui/signin/SignInActivity.kt
+++ app/src/main/java/com/mashup/torchlight/ui/signin/SignInActivity.kt
@@ -3,11 +3,30 @@ package com.mashup.torchlight.ui.signin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mashup.torchlight.R
+import com.mashup.torchlight.ui.signup.SignUpActivity
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
+        btnSignIn.setOnClickListener {
+            // sign in
+        }
+
+        tvSignUp.setOnClickListener {
+            startActivity<SignUpActivity>()
+        }
+
+        tvFindPw.setOnClickListener {
+            toast("Find password...")
+        }
     }
 }
